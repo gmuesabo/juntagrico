ab254f2ce1b8133efed0c33e155fe42b23234937
pascalfree <pascal.free@ihdg.ch>
2026-07-24 05:09:38 +0200

    added --exclude-former option for share import

--- gmuesabo/management/commands/import_shares.py
+++ gmuesabo/management/commands/import_shares.py
@@ -25,7 +25,15 @@ class Command(BaseCommand):
             help='Delete existing shares before import',
         )
 
-    def handle(self, file, *args, clear=False, **options):
+        parser.add_argument(
+            '-e' '--exclude-former',
+            action='store_true',
+            dest='exclude_former',
+            default=False,
+            help='Do not import paid back shares',
+        )
+
+    def handle(self, file, *args, clear=False, exclude_former=False, **options):
         with open(file, 'rb') as fh:
             imported_data = tablib.import_set(fh, skip_lines=4)
 
@@ -37,18 +45,34 @@ class Command(BaseCommand):
 
             today = datetime.date.today()
             count_shares = 0
+            full_name = street = zipcode = location = None
             for row in imported_data:
                 if row[1] == 'vakant':
                     continue
 
                 number = row[0]
-                zipcode, _, location = (row[6] or '').partition(' ')
                 first_name = (row[2] or '-').strip()
                 last_name = row[1].strip()
+                if full_name != f'{first_name} {last_name}':
+                    full_name = f'{first_name} {last_name}'
+                    zipcode, _, location = (row[6] or '').partition(' ')
+                    street = row[5]
+
+                payback_date = None
+                payback = str(row[10]).lower()
+                if 'spende' in payback or 'ausbez' in payback:
+                    result = PAYBACK_REGEX.search(payback)
+                    if result:
+                        if exclude_former:
+                            continue
+                        payback_date = datetime.date(
+                            2000 + int(result.group(3)), int(result.group(2)), int(result.group(1))
+                        )
+
                 try:
                     member = Member.objects.get_or_create(first_name=first_name or '-', last_name=last_name, defaults={
                         'email': f'share{number}@juntagrico.id',
-                        'addr_street': row[5] or '?',
+                        'addr_street': street or '?',
                         'addr_zipcode': zipcode,
                         'addr_location': location,
                         'phone': '-',
@@ -66,25 +90,17 @@ class Command(BaseCommand):
                         paid_date = datetime.datetime.strptime(paid_date, "%d.%m.%Y").date()
                     except ValueError:
                         paid_date = datetime.datetime.strptime(paid_date[4:].strip(), "%d.%m.%y").date()
-                share = Share.objects.create(
+                Share.objects.create(
                     number=number,
                     member=member,
                     paid_date=paid_date,
                     issue_date=paid_date,
                     booking_date=paid_date,
+                    cancelled_date=payback_date,
+                    termination_date=payback_date,
+                    payback_date=payback_date,
                     notes=f'{row[3] or ""}\n{row[4] or ""}\n{row[10] or ""}'
                 )
                 count_shares += 1
-                payback = str(row[10]).lower()
-                if 'spende' in payback or 'ausbez' in payback:
-                    result = PAYBACK_REGEX.search(payback)
-                    if result:
-                        payback_date = datetime.date(
-                            2000 + int(result.group(3)), int(result.group(2)), int(result.group(1))
-                        )
-                        share.cancelled_date = payback_date
-                        share.termination_date = payback_date
-                        share.payback_date = payback_date
-                        share.save()
 
             print('imported {} shares'.format(count_shares))
